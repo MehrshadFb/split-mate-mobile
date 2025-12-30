a6f9e6c5c0c964eb5ab619b279c783fbe3a31c36
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2025-12-30 11:11:32 -0500

    cleanup: features/setting

--- split-mate-app/src/features/settings/constants/appInfo.ts
+++ split-mate-app/src/features/settings/constants/appInfo.ts
@@ -1,9 +1,5 @@
-// src/features/settings/constants/appInfo.ts
-// Application version and build information
-
 export const APP_VERSION = "1.0.0";
 export const APP_BUILD = "1";
-
 export const APP_LINKS = {
   github: "https://github.com/MehrshadFb/SplitMate-Mobile",
   privacy: "https://yourdomain.com/privacy",
--- split-mate-app/src/features/settings/index.ts
+++ split-mate-app/src/features/settings/index.ts
@@ -1,6 +1,3 @@
-// src/features/settings/index.ts
-// Barrel export for settings feature
-
 export * from "./components";
 export * from "./constants/appInfo";
 
