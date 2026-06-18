ba52f4da58687ef88fbcae48ca9512ab13c2309f
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2026-06-18 01:54:16 -0400

    fix: read app version from native metadata

--- split-mate-app/src/features/settings/constants/appInfo.ts
+++ split-mate-app/src/features/settings/constants/appInfo.ts
@@ -1,5 +1,19 @@
-export const APP_VERSION = "1.0.0";
-export const APP_BUILD = "1";
+import * as Application from "expo-application";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
+
+const appConfigBuild = Platform.select({
+  ios: Constants.expoConfig?.ios?.buildNumber,
+  android: Constants.expoConfig?.android?.versionCode?.toString(),
+});
+
+export const APP_VERSION =
+  Application.nativeApplicationVersion ??
+  Constants.expoConfig?.version ??
+  "Unknown";
+export const APP_BUILD =
+  Application.nativeBuildVersion ?? appConfigBuild ?? "Unknown";
+
 export const APP_LINKS = {
   github: "https://github.com/MehrshadFb/SplitMate-Mobile",
   privacy: "https://split-mate-mobile-landing.vercel.app/privacy-policy",
